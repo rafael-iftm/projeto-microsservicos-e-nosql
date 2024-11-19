e544121f9012d47d3eecab7b0f49f732f0cdd188
Rafael Prado <142061101+rafael-iftm@users.noreply.github.com>
2024-11-18 22:16:03 -0300

    Aula 04 - Finalizando a Implementação de UserAPI

--- userapi/README.md
+++ userapi/README.md
@@ -1,1 +1,3 @@
 ### Aula 03: [Aplicação com Spring Boot](https://docs.google.com/document/d/1GT13KEFk6uu967JrGehT-kiBdf7pznd2WnSWAoAqXl0/edit?usp=sharing)
+
+### Aula 04: [Finalizando a Implementação de UserAPI](https://docs.google.com/document/d/18BnM_5Cbz0c9rrMYT7nE3wO6e_HkDgWyOmT8mCahgBM/edit?usp=sharing)
--- userapi/src/main/java/com/example/userapi/controller/UserController.java
+++ userapi/src/main/java/com/example/userapi/controller/UserController.java
@@ -6,17 +6,24 @@ import jakarta.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    // Lista estática de usuários para fins de demonstração
     public static List<UserDTO> usuarios = new ArrayList<>();
 
-    // Inicializa a lista de usuários após a criação do bean
     @PostConstruct
     public void initiateList() {
         UserDTO user1 = new UserDTO("Eduardo", "123", "Rua a", "eduardo@email.com", "1234-3454", LocalDateTime.now());
@@ -28,9 +35,31 @@ public class UserController {
         usuarios.add(user3);
     }
 
-    // Retorna a lista de usuários cadastrados
     @GetMapping
     public List<UserDTO> getUsers() {
         return usuarios;
+    }
+
+    @GetMapping("/{cpf}")
+    public UserDTO getUserByCpf(@PathVariable String cpf) {
+        return usuarios
+                .stream()
+                .filter(userDTO -> userDTO.getCpf().equals(cpf))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO addUser(@RequestBody @Valid UserDTO userDTO) {
+        userDTO.setDataCadastro(LocalDateTime.now());
+        usuarios.add(userDTO);
+        return userDTO;
+    }
+
+    @DeleteMapping("/{cpf}")
+    public boolean deleteUser(@PathVariable String cpf) {
+        return usuarios
+            .removeIf(userDTO -> userDTO.getCpf().equals(cpf));
     }
 }
